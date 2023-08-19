43233c756abedac4c5a6b6e0a0a54de5002aaf0b
Ilija Pejanović <ikadvm96@gmail.com>
2023-08-19 13:24:58 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/globals.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { Home } from './components/home/Home';
 
 export const App: React.FC = () => {
