8e164a410d53c1a2c7e1ea3144eae8195a629fcd
Ilija Pejanović <ikadvm96@gmail.com>
2023-08-23 22:08:35 +0200

    fixed a hamburger button bug in mobile view

--- src/components/nav/Heading.tsx
+++ src/components/nav/Heading.tsx
@@ -1,41 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
+import styles from './heading.module.scss';
+import { MyLinks } from './components/MyLinks';
+import { useState, useEffect, useRef } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { RiMenuUnfoldLine } from 'react-icons/ri';
-import { MyLinks } from './components/MyLinks';
-import styles from './heading.module.scss';
 
 export const Heading = () => {
   const [openMobile, setOpenMobile] = useState(false);
-  const toggleref = useRef<HTMLButtonElement>(null); // Use React.RefObject<HTMLButtonElement>
+  const handleMobileViewOpen = () => {
+    setOpenMobile(true);
+  };
+  const handleMobileViewClose = () => {
+    setOpenMobile(false);
+  };
+  const toggleref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (toggleref.current
-        && !toggleref.current.contains(event.target as Node)) {
+      if (toggleref.current && !toggleref.current.contains(event.target as Node)) {
         setOpenMobile(false);
       }
     };
-
     document.addEventListener('mousedown', handleClickOutside);
-
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [toggleref]);
-
-  const handleMobileViewOpen = () => {
-    setOpenMobile(true);
-  };
-
-  const handleMobileViewClose = () => {
-    setOpenMobile(false);
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.key === 'Enter' || e.key === ' ') {
-      handleMobileViewClose();
-    }
-  };
 
   return (
     <header className={styles.heading}>
@@ -46,22 +35,22 @@ export const Heading = () => {
         ) : (
           <RiMenuUnfoldLine onClick={handleMobileViewOpen} />
         )}
-        <button
-          className={`${styles.navbarMobileOpen} ${
-            openMobile ? styles.navbarMobileOpenActive : ''
-          }`}
-          type="button"
+        <div
+          className={
+            openMobile
+              ? styles.navbarMobileOpenActive
+              : styles.navbarMobileOpen
+          }
           onClick={handleMobileViewClose}
-          onKeyDown={handleKeyDown} // Add a keyDown event handler for accessibility
           ref={toggleref}
         >
           <nav>
-            <a href="#home">Home</a>
+            <a href="#">Home</a>
             <a href="#about">About</a>
             <a href="#projects">Projects</a>
             <a href="#contact">Contact</a>
           </nav>
-        </button>
+        </div>
       </div>
     </header>
   );
